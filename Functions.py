f348ac785a51721bff3090ae1103f45220be1a1e
Astronom5 <mateusz.pawelec@zhr.pl>
2022-03-16 17:30:05 +0100

    Forced user to give proper data, added gitignore Integrated main with functions

--- Functions.py
+++ Functions.py
@@ -1,10 +1,9 @@
 def add_data():
     data = []
     for counter in range(6):
-        collected_data=int(input(f"Podaj {counter+1} liczbę do losowania"))
+        collected_data=int(input(f"Podaj {counter+1} liczbę do losowania: "))
+        if collected_data not in range (1,50) or collected_data in data:
+            while collected_data not in range (1,50) or collected_data in data:
+                collected_data=int(input(f"Podaj {counter+1} liczbę do losowania: "))
         data.append(collected_data)
     return set(data)
-
-
-my_data=add_data()
-print('Skończyłem')
---
+++ Main.py
@@ -0,0 +1,2 @@
+from Functions import add_data
+my_data=add_data()
