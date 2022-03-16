643caa5bc9202cee6cffe7a26dfabe8a48ceb0ac
Astronom5 <mateusz.pawelec@zhr.pl>
2022-03-16 22:10:13 +0100

    Added some new functions to calculate

--- Functions.py
+++ Functions.py
@@ -19,9 +19,30 @@ def lottery_draw():
         lottery_numbers.add(randint(1,49))
     return lottery_numbers
 
+
 def win_classification( typed_numbers, lottery_numbers):
     win_type = len(typed_numbers.intersection(lottery_numbers))
     if win_type>2:
         return win_type
     else:
         return 0
+
+
+def age_counter(number_of_draws):
+    added_age = number_of_draws/52
+    return added_age
+
+
+def cost(number_of_draws):
+    cost=number_of_draws*3
+    return cost
+
+
+def income_func(win_type_3 = 0, win_type_4 = 0 ,win_type_5 = 0, win_type_6 = 1):
+    income = win_type_3*16 + win_type_4*500 + win_type_5*5000 + win_type_6*2000000
+    return income
+
+
+def profit_func(income, cost):
+    profit=income-cost
+    return profit
