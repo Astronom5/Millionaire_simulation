559bf983015dbe39bb06b3e9c8d27b0ff6c5772a
Astronom5 <mateusz.pawelec@zhr.pl>
2022-03-16 16:59:24 +0100

    Fixed function that collects data from user.

--- Functions.py
+++ Functions.py
@@ -1,8 +1,10 @@
 def add_data():
     data = []
     for counter in range(6):
-        data.append=int(input(f"Podaj {counter} liczbę do losowania"))
+        collected_data=int(input(f"Podaj {counter+1} liczbę do losowania"))
+        data.append(collected_data)
     return set(data)
 
 
-my_data=add_data
+my_data=add_data()
+print('Skończyłem')
